a2b211aba869779f64b0c3ee058f269517f63c86
TKearney1 <taylor.kearney1@monash.edu>
2024-09-19 16:33:38 +1000

    Updated fixed formulation in advection_diffusion_mixed.py

--- advection_diffusion_mixed.py
+++ advection_diffusion_mixed.py
@@ -6,7 +6,7 @@ parameters["form_compiler"]["quadrature_degree"] = 4
 
 
 # output file
-output_file = XDMFFile("outputs/all_sols.xdmf")
+output_file = XDMFFile("outputs/exp_boundary_sols.xdmf")
 output_file.parameters['rewrite_function_mesh']=False
 output_file.parameters["flush_output"] = True
 output_file.parameters["functions_share_mesh"] = True
@@ -17,7 +17,10 @@ bdry = MeshFunction("size_t", mesh, "meshes/expBoundary_facet_region.xml")
 r2, r1 = SpatialCoordinate(mesh)
 
 # mesh labels
-right = 21; top = 22; left = 23; bottom = 24
+right = 21
+top = 22
+left = 23
+bottom = 24
 
 # ******* Model constants ****** #
 
@@ -25,20 +28,20 @@ c = Constant(1.)
 V = Constant(1.)
 sigma = Constant(0.05)
 gamma = Constant(1.0)
-D2 = Constant(1)
-D1 = Constant(1)
+D2 = Constant(0.001)
+D1 = Constant(0.001)
 r2_vec = Constant((1, 0))
 r1_vec = Constant((0, 1))
 f = Constant(0.)
 
 # ********** Time constants ********* #
-t = 0.; dt = 0.1; tfinal = 10.0
-
-
+t = 0.
+dt = 0.001
+tfinal = 0.01
 
 
 # ********* Finite dimensional spaces ********* #
-deg=1
+deg = 2
 P0 = FiniteElement('DG',mesh.ufl_cell(),deg-1)
 RT1 = FiniteElement('RT',mesh.ufl_cell(),deg)
 P1 = FiniteElement('CG', mesh.ufl_cell(), deg)
@@ -46,67 +49,58 @@ mixed_space = FunctionSpace(mesh, MixedElement([P0, RT1, P1]))
 
 # ********* test and trial functions ****** #
 v1, tau, v2 = TestFunctions(mixed_space)
-u  = Function(mixed_space)
+u = Function(mixed_space)
 du = TrialFunction(mixed_space)
 p, s, q = split(u)
 
 # ********* initial and boundary conditions ******** #
-p_initial = Expression("c/V*exp(-4/3*pi*c*pow(x[0],3))*(1-o/x[1]*exp(-4/3*pi*g*pow(x[0],3)))", degree = 1, c=c, V=V, o = sigma, g = gamma, domain = mesh)
-
-q_initial = Expression("1/(4*pi*x[1]*V*(c+g))*exp(-4/3*pi*pow(x[0],3)*(c+g))*(exp(4/3*pi*pow(x[0],3)*g)*x[1]*(c+g)-c*o)", degree = 1, c=c, V=V, o = sigma, g = gamma, domain = mesh)
-
+p_initial = Expression("c/V*exp(-4/3*pi*c*pow(x[0],3))*(1-o/x[1]*exp(-4/3*pi*g*pow(x[0],3)))", degree=deg-1, c=c, V=V,
+                       o=sigma, g=gamma, domain=mesh)
 p_old = interpolate(p_initial, mixed_space.sub(0).collapse())
-q_old = interpolate(q_initial, mixed_space.sub(2).collapse())
-    
-p_right = Expression("c/V*exp(-4/3*pi*c*pow(x[0],3))", degree = 2, c=c, V=V, o = sigma, g = gamma, domain = mesh)
-p_top = Expression("c/V*exp(-4/3*pi*c*pow(x[0],3))", degree = 2, c=c, V=V, o = sigma, g = gamma, domain = mesh)
+
+p_right = Expression("c/V*exp(-4/3*pi*c*pow(x[0],3))*(1-o/x[1]*exp(-4/3*pi*g*pow(x[0],3)))", degree=deg-1, c=c, V=V,
+                     o=sigma, g=gamma, domain=mesh)
+p_top = Expression("c/V*exp(-4/3*pi*c*pow(x[0],3))*(1-o/x[1]*exp(-4/3*pi*g*pow(x[0],3)))", degree=deg-1, c=c, V=V,
+                   o=sigma, g=gamma, domain=mesh)
 p_bottom = Constant(0.)
     
 # as the formulation for p-fluxp is mixed, the boundary condition for p becomes natural and the boundary condition for the flux becomes essential (dirichlet) 
 
 # the formulation for q is primal, so the Dirichlet conditions remain so
-q_right = Expression('(1/(4*pi*x[1]*V*(c+g)))*exp(-4/3*pi*pow(x[0],3)*(c+g))*(x[1]*(c+g)*exp(4/3*pi*pow(x[0],3)*g)-c*o)', degree = 2, c=c, V=V, o = sigma, g = gamma, domain = mesh)
-q_top = Expression('(1/(4*pi*x[1]*V*(c+g)))*exp(-4/3*pi*pow(x[0],3)*(c+g))*(x[1]*(c+g)*exp(4/3*pi*pow(x[0],3)*g)-c*o)', degree = 2, c=c, V=V, o = sigma, g = gamma, domain = mesh)
+q_right = Expression('(1/(4*pi*x[1]*V*(c+g)))*exp(-4/3*pi*pow(x[0],3)*(c+g))*(x[1]*(c+g)*exp(4/3*pi*pow(x[0],3)*g)-c*o)'
+                     , degree=deg, c=c, V=V, o=sigma, g=gamma, domain=mesh)
 q_right_boundary_condition = DirichletBC(mixed_space.sub(2), q_right, bdry, right)
-q_top_boundary_condition = DirichletBC(mixed_space.sub(2), q_top, bdry, top)
 
 #Boundary conditions for s are complementary to those of p:
 s_left_boundary_condition = DirichletBC(mixed_space.sub(1), Constant((0, 0)), bdry, left)
 
 # here we only list the Dirichlet ones 
-bc = [s_left_boundary_condition, q_right_boundary_condition, q_top_boundary_condition]
+bc = [s_left_boundary_condition, q_right_boundary_condition]
 
     
 # (approximate) steady-state solutions used for comparison
-p_steady_state = Expression("c/V*exp(-4/3*pi*c*pow(x[0],3))*(1-(o/x[1])*exp(-4/3*pi*g*pow(x[0],3)))", degree = 2, c=c, V=V, o = sigma, g = gamma, domain = mesh)
-q_steady_state = Expression("c/(4*pi*V)*exp(-4/3*pi*c*pow(x[0],3))", degree = 2, c=c, V=V, o = sigma, g = gamma, domain = mesh)
+p_steady_state = Expression("c/V*exp(-4/3*pi*c*pow(x[0],3))*(1-(o/x[1])*exp(-4/3*pi*g*pow(x[0],3)))", degree=deg-1, c=c,
+                            V=V, o=sigma, g=gamma, domain=mesh)
 
 p_approx = interpolate(p_steady_state, mixed_space.sub(0).collapse())
-q_approx = interpolate(q_steady_state, mixed_space.sub(2).collapse())
 
 
 def Gstar(p,q):
-    return conditional(gt(q,0),D2*r2**2*p**2/q,0)
+    return conditional(gt(q,0), (r2**2*p**2)/q,0)
             
 # ********* Weak forms ********* #
 
 n = FacetNormal(mesh)
 dMatrix = as_tensor([[D2,0],[0,D1]])
 weight = (4*pi*r1*r2)**2
-aux = 2*(r2_vec / r2 + r1_vec / r1)
 
 FF = (p - p_old) / dt * v1 * weight * dx \
-     + (div(s)+ dot(aux,s)) * v1 * weight * dx \
-     + dot(s + Gstar(p,q) * r2_vec, tau) * weight * dx \
-     - p * (div(dMatrix*tau) + dot(aux,dMatrix*tau)) * weight * dx \
-     + p_bottom * dot(dMatrix * tau, n) * weight * ds(bottom) \
-     + p_right * dot(dMatrix * tau, n) * weight * ds(right) \
-     + p_top * dot(dMatrix * tau, n) * weight * ds(top) \
-     + (q - q_old) / dt * v2 * weight * dx \
-     + dot(dMatrix*grad(q),grad(v2)) * weight * dx \
-     + (D2*4./r2) * Dx(q,0) * v2 * weight * dx \
-     + r2 ** 2 * Gstar(p,q) * v2 * weight * dx \
-     - dot(D1 * Dx(q,1) * v2 * r1_vec, n) * weight * ds(bottom)
+     + ((D1/r1**2)*Dx(r1**2*(dot(s,r1_vec)),1) + (D2/r2**2)*Dx(r2**2*(dot(s,r2_vec)),0)) * v1 * weight * dx \
+     + dot(s + (Gstar(p, q)*r2_vec), tau) * weight * dx \
+     - p * ((1/r1**2)*Dx(r1**2*(dot(tau,r1_vec)),1) + (1/r2**2)*Dx(r2**2*(dot(tau,r2_vec)),0)) * weight * dx \
+     + p_right * dot(tau, n) * weight * ds(right) \
+     + p_top * dot(tau, n) * weight * ds(top) \
+     + Dx(q, 0)*v2*weight*dx + r2**2*p*v2*weight*dx
 
 
 # Last term is from the boundary condition for q which states dq/dr2 = 0 on the inner boundary.
@@ -118,7 +112,7 @@ solver.parameters['nonlinear_solver']                    = 'newton'
 solver.parameters['newton_solver']['linear_solver']      = 'mumps'
 solver.parameters['newton_solver']['absolute_tolerance'] = 1e-7
 solver.parameters['newton_solver']['relative_tolerance'] = 1e-7
-solver.parameters['newton_solver']['maximum_iterations'] = 50
+solver.parameters['newton_solver']['maximum_iterations'] = 10
 
 
 while (t <=tfinal):
@@ -138,6 +132,8 @@ while (t <=tfinal):
     output_file.write(difp,t)
     # Update the solution for next iteration
     assign(p_old, p_h)
-    assign(q_old, q_h)
 
     t += dt
+
+total_flux = assemble(dot(dMatrix*s_h, n)*weight*ds(bottom))
+print(f'Total flux: {total_flux}')
