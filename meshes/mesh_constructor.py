860dd6a2d7efc2f8ffe71b906036e8e0578ccfbd
TKearney1 <taylor.kearney1@monash.edu>
2025-07-21 12:21:01 +1000

    Untracking unnecessary files.

--- meshes/mesh_constructor.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-
-edp_file_path = "meshWithExpReal.edp"
-bashCommand = ["Freefem++",  edp_file_path, "0.1", "1"]
-process = subprocess.Popen(bashCommand, stdout=subprocess.PIPE)
-#output, error = process.communicate()
-#print(output)
